a11748a933f9a8bf37614d97d26668e8fe76cdba
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-05-02 17:15:12 +0430

    minigame map.config

--- scripts/match.sh
+++ scripts/match.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
+# curl "https://raw.githubusercontent.com/SharifAIChallenge/final-judgment/master/resources/map.config" > /tmp/match/map.config
+curl "https://raw.githubusercontent.com/SharifAIChallenge/AIC21-Minigame/main/map.config" > /tmp/match/map.config
 (cd /tmp/match && java -jar /usr/local/match/match.jar $@)
